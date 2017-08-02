da3167004c6cc6c232ec948550f176f919ab3482
Raylin51 <652112498@qq.com>
2017-08-02 13:38:06 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var superagent = require('superagent');
+var app = express();
+
+app.get('/img', function(req, res, next) {
+    res.writeHead(200, {
+        'Content-Type': 'image/*'
+    });
+    let url = req.query.url;
+    if (!url) {
+        res.send("");
+        return false;
+    }
+    superagent.get(req.query.url)
+    .set('Referer', '')
+    .set("User-Agent",
+        'User-Agent:Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36 Core/1.47.933.400 QQBrowser/9.4.8699.400'
+    )
+    .end(function(err, result) {
+        if (err) {
+            //res.send(err);
+            return false;
+        }
+        res.end(result.body);
+        return;
+    });
+});
+
+app.listen(9999, function() {
+    console.log('App listening on 9999');
+});
